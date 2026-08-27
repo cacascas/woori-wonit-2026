3d47ddb15863b4f8a839abc30a59cbc87e7b7984
cacascas <dlcodud156@gmail.com>
2026-08-27 15:49:42 +0900

    feat. create new file on frontend

---
+++ 01-frontend/01_aiCoding.js
@@ -0,0 +1,19 @@
+//두 수를 더하는 함수입니다.
+function add(a, b) {
+	return a + b;
+}
+
+//두 수를 빼는 함수입니다.
+function subtract(a, b) {
+    return a - b;
+}
+
+//두 수를 곱하는 함수입니다.
+function multiply(a, b) {
+    return a * b;
+}
+
+//두 수를 나누는 함수입니다.
+function divide(a, b) {
+    return a / b;
+}
